97a9373a39dd449554f773a3e045859005a47a2c
Adam Gruhn <adam.gruhn@my.simpson.edu>
2021-09-02 15:14:27 -0500

    Lab 1

---
+++ Lab 01 - First Program/lab_01.py
@@ -0,0 +1,4 @@
+print("Hello, my name is Adam Gruhn")
+print("This is the first class I've taken with any programming")
+print("Right now this doesn't seem too hard")
+print("In about a week my brain will be going\n\"AAAAAAAAAA\nAAAAAAAAAAAAAA\nAAAAAAAAAHHHHH!!!!\"")
