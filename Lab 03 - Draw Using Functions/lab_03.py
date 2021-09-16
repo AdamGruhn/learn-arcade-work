91368f5d8b1c0e7ccaaf49f5e6b17c73383ab50d
Adam Gruhn <adam.gruhn@my.simpson.edu>
2021-09-16 15:27:56 -0500

    Cleaning up lab 3

--- Lab 03 - Draw Using Functions/lab_03.py
+++ Lab 03 - Draw Using Functions/lab_03.py
@@ -15,47 +15,23 @@ yellow = (255, 209, 0)
 
 
 # To make the blue waves with the white background
-def draw_wave_blue(left_x, bottom_y):
-    arcade.draw_lrtb_rectangle_filled(left_x, screen_width, (bottom_y + 60), bottom_y, blue)
-    arcade.draw_arc_filled(left_x, bottom_y, 200, 200, blue, 0, 180)
-    arcade.draw_arc_filled((left_x + 240), bottom_y, 200, 200, blue, 0, 180)
-    arcade.draw_arc_filled((left_x + 480), bottom_y, 200, 200, blue, 0, 180)
-    arcade.draw_arc_filled((left_x + 720), bottom_y, 200, 200, blue, 0, 180)
-    arcade.draw_arc_filled((left_x + 960), bottom_y, 200, 200, blue, 0, 180)
-    arcade.draw_arc_filled((left_x + 1200), bottom_y, 200, 200, blue, 0, 180)
+def draw_wave(left_x, bottom_y, color, background_color):
+    arcade.draw_lrtb_rectangle_filled(left_x, screen_width, (bottom_y + 60), bottom_y, color)
+    arcade.draw_arc_filled(left_x, bottom_y, 200, 200, color, 0, 180)
+    arcade.draw_arc_filled((left_x + 240), bottom_y, 200, 200, color, 0, 180)
+    arcade.draw_arc_filled((left_x + 480), bottom_y, 200, 200, color, 0, 180)
+    arcade.draw_arc_filled((left_x + 720), bottom_y, 200, 200, color, 0, 180)
+    arcade.draw_arc_filled((left_x + 960), bottom_y, 200, 200, color, 0, 180)
+    arcade.draw_arc_filled((left_x + 1200), bottom_y, 200, 200, color, 0, 180)
     y = bottom_y + 65
-    arcade.draw_arc_filled((left_x + 120), y, 80, 60, white, 180, 360)
-    arcade.draw_arc_filled((left_x + 360), y, 80, 60, white, 180, 360)
-    arcade.draw_arc_filled((left_x + 600), y, 80, 60, white, 180, 360)
-    arcade.draw_arc_filled((left_x + 840), y, 80, 60, white, 180, 360)
-    arcade.draw_arc_filled((left_x + 1080), y, 80, 60, white, 180, 360)
-
-
-# To make the white waves with the blue background
-def draw_wave_white(left_x, bottom_y):
-    arcade.draw_lrtb_rectangle_filled(left_x, screen_width, (bottom_y + 60), bottom_y, white)
-    arcade.draw_arc_filled(left_x, bottom_y, 200, 200, white, 0, 180)
-    arcade.draw_arc_filled((left_x + 240), bottom_y, 200, 200, white, 0, 180)
-    arcade.draw_arc_filled((left_x + 480), bottom_y, 200, 200, white, 0, 180)
-    arcade.draw_arc_filled((left_x + 720), bottom_y, 200, 200, white, 0, 180)
-    arcade.draw_arc_filled((left_x + 960), bottom_y, 200, 200, white, 0, 180)
-    arcade.draw_arc_filled((left_x + 1200), bottom_y, 200, 200, white, 0, 180)
-    y = bottom_y + 65
-    arcade.draw_arc_filled((left_x + 120), y, 80, 60, blue, 180, 360)
-    arcade.draw_arc_filled((left_x + 360), y, 80, 60, blue, 180, 360)
-    arcade.draw_arc_filled((left_x + 600), y, 80, 60, blue, 180, 360)
-    arcade.draw_arc_filled((left_x + 840), y, 80, 60, blue, 180, 360)
-    arcade.draw_arc_filled((left_x + 1080), y, 80, 60, blue, 180, 360)
-
-
-# This is to make the top wave match the sky in the background
-def top_wave_background(left_x, bottom_y):
-    y = bottom_y + 65
-    arcade.draw_arc_filled((left_x + 120), y, 80, 60, red, 180, 360)
-    arcade.draw_arc_filled((left_x + 360), y, 80, 60, red, 180, 360)
-    arcade.draw_arc_filled((left_x + 600), y, 80, 60, yellow, 180, 360)
-    arcade.draw_arc_filled((left_x + 840), y, 80, 60, red, 180, 360)
-    arcade.draw_arc_filled((left_x + 1080), y, 80, 60, red, 180, 360)
+    arcade.draw_arc_filled((left_x + 120), y, 80, 60, background_color, 180, 360)
+    arcade.draw_arc_filled((left_x + 360), y, 80, 60, background_color, 180, 360)
+    arcade.draw_arc_filled((left_x + 840), y, 80, 60, background_color, 180, 360)
+    arcade.draw_arc_filled((left_x + 1080), y, 80, 60, background_color, 180, 360)
+    if background_color == red:
+        arcade.draw_arc_filled((left_x + 600), y, 80, 60, yellow, 180, 360)
+    else:
+        arcade.draw_arc_filled((left_x + 600), y, 80, 60, background_color, 180, 360)
 
 
 def draw_sun_body(center_x, center_y, radius):
@@ -89,12 +65,11 @@ def main():
     draw_sun_rays(600, 250, 10, 350, 30)
 
     # Making the waves starting from the top
-    draw_wave_white(0, 180)
-    top_wave_background(0, 180)
-    draw_wave_blue(0, 120)
-    draw_wave_white(0, 60)
-    draw_wave_blue(0, 0)
-    draw_wave_white(0, -60)
+    draw_wave(0, 180, white, red)
+    draw_wave(0, 120, blue, white)
+    draw_wave(0, 60, white, blue)
+    draw_wave(0, 0, blue, white)
+    draw_wave(0, -60, white, blue)
 
     # Adding some birds
     draw_bird(900, 450, black)
