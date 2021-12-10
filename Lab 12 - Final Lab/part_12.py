51bf40287cccbafd32979a68fd6adadf7f779631
Adam Gruhn <adam.gruhn@my.simpson.edu>
2021-12-10 15:14:59 -0600

    test file has my start screen layout that I want, made a few improvements to the actual final part

--- Lab 12 - Final Lab/part_12.py
+++ Lab 12 - Final Lab/part_12.py
@@ -13,55 +13,39 @@ https://tetris.fandom.com/wiki/Scoring
 """
 To Do
 
-DONE DONE Figure out why the pieces appear at the top of the screen
-DONE DONE Make the screen larger than just the play area
-DONE DONE Make the bottom layer of 1's below the screen
-DONE DONE Make the bottom layer of 1's be DARK_GRAY without changing the colors of the tiles
-
-DONE DONE Game Over Screen
-DONE DONE Add a scoring system
-DONE DONE Add a level system
-DONE DONE Able to pause
-DONE DONE Able to unpause
-DONE DONE Line Clear Counter
 Start screen with level selects - Sprites to click to select start level instead of typing
-Add a next box
-Counter for number of times each piece appeared
 Sound effects on line clears
+Counter for number of times each piece appeared
+Slight delay after line clears
 Create a leaderboard system at some point?
-
-DONE DONE Made the speed scale with level
-
 """
-# Set how many rows and columns we will have
+# Set how many rows and columns we will have and how big each square will be
 ROW_COUNT = 21
 COLUMN_COUNT = 10
-# This sets the WIDTH and HEIGHT of each grid location
-# Only one variable because they are squares
 SIZE = 30
-# This sets the margin between each cell
-# and on the edges of the screen.
+# This sets the margin between each cell and edge of the screen
 MARGIN = 5
-# Do the math to figure out the play screen dimensions
+# How many different levels to choose to start from
+START_LEVELS = 10
+LEVEL_SELECT_SIZE = SIZE * 2 + MARGIN
+# Do the math to figure out the play screen dimensions and window dimensions
 PLAY_SCREEN_WIDTH = (SIZE + MARGIN) * COLUMN_COUNT + MARGIN
 PLAY_SCREEN_HEIGHT = (SIZE + MARGIN) * ROW_COUNT + MARGIN
 SCREEN_HEIGHT = (SIZE + MARGIN) * (ROW_COUNT + 3) + MARGIN
 SCREEN_WIDTH = 3 * PLAY_SCREEN_WIDTH
 
-
 # Colors in order L, I, T, S, Z, O, J
 colors = [arcade.color.DARK_GRAY,
-          arcade.color.PURPLE,
           arcade.color.DARK_RED,
-          arcade.color.YELLOW,
-          arcade.color.GREEN,
-          arcade.color.CYAN,
+          arcade.color.MIKADO_YELLOW,
+          arcade.color.MIKADO_YELLOW,
+          arcade.color.DARK_RED,
           arcade.color.BLUE,
-          arcade.color.WHITE,
+          arcade.color.MIKADO_YELLOW,
+          arcade.color.BLUE,
           arcade.color.BLACK]
 
-# Making the shapes in their own matrices
-# Same order, L, I, T, S, Z, O, J
+# Matrices in order, L, I, T, S, Z, O, J
 TETROMINOES = [
     [[1, 1, 1],
      [1, 0, 0]],
@@ -159,6 +143,8 @@ class MyGame(arcade.Window):
         self.game_over = False
         self.paused = 1
         self.board_sprite_list = None
+        self.down_pressed = False
+        self.down_timer = 0
 
         self.tile = None
         self.tile_x = 0
@@ -184,7 +170,6 @@ class MyGame(arcade.Window):
         self.tile_y = 0
         # Makes a tile that I can display in the "Next Box"
         self.next_tile = random.choice(TETROMINOES)
-        print(self.next_tile)
 
         if check_collision(self.board, self.tile, (self.tile_x, self.tile_y)):
             self.game_over = True
@@ -221,6 +206,8 @@ class MyGame(arcade.Window):
                                                        self.tile_y)):
                 self.board = join_matrices(self.board, self.tile,
                                            (self.tile_x, self.tile_y))
+                # Resets the down button everytime a piece hits another piece
+                self.down_pressed = False
                 while True:
                     # Line clear code
                     for i, row in enumerate(self.board[:-1]):
@@ -228,7 +215,6 @@ class MyGame(arcade.Window):
                             self.board = remove_row(self.board, i)
                             self.line_clears += 1
                             self.piece_clear += 1
-
                             break
                     else:
                         break
@@ -236,20 +222,20 @@ class MyGame(arcade.Window):
                     self.score += (self.current_level + 1) * 1200
                     # Each of these if statements makes it so that you
                     # Don't reach a new level until your line count is high enough
-                    if self.line_clears >= self.start_level:
-                        self.current_level = self.start_level + self.line_clears // 10
+                    if self.line_clears // 10 >= self.start_level:
+                        self.current_level = self.line_clears // 10
                 if self.piece_clear == 3:
                     self.score += (self.current_level + 1) * 300
-                    if self.line_clears >= self.start_level:
-                        self.current_level = self.start_level + self.line_clears // 10
+                    if self.line_clears // 10 >= self.start_level:
+                        self.current_level = self.line_clears // 10
                 if self.piece_clear == 2:
                     self.score += (self.current_level + 1) * 100
-                    if self.line_clears >= self.start_level:
-                        self.current_level = self.start_level + self.line_clears // 10
+                    if self.line_clears // 10 >= self.start_level:
+                        self.current_level = self.line_clears // 10
                 if self.piece_clear == 1:
                     self.score += (self.current_level + 1) * 40
-                    if self.line_clears >= self.start_level:
-                        self.current_level = self.start_level + self.line_clears // 10
+                    if self.line_clears // 10 >= self.start_level:
+                        self.current_level = self.line_clears // 10
 
                 self.update_board()
                 self.new_tile()
@@ -281,12 +267,17 @@ class MyGame(arcade.Window):
     def on_update(self, dt):
         """Basically the game speed"""
         self.frame_count += 1
+        self.down_timer += dt
         # Speed scales with each level that you are on
         if self.current_level <= 58:
             if self.frame_count % (59 // (self.current_level + 1)) == 0:
                 self.fall()
         # When current level is larger than 58 it divides by 0 and breaks
         elif self.current_level > 58:
+            if self.frame_count % 1 == 0:
+                self.fall()
+        # Makes the piece keep moving when you hold down
+        if self.down_pressed and self.down_timer > 0:
             if self.frame_count % 1 == 0:
                 self.fall()
 
@@ -317,9 +308,14 @@ class MyGame(arcade.Window):
         elif key == arcade.key.UP or key == arcade.key.L:
             self.rotate_tile_clockwise()
         elif key == arcade.key.DOWN or key == arcade.key.S:
-            self.fall()
+            self.down_pressed = True
+            self.down_timer = 0
         elif key == arcade.key.P:
             self.paused *= -1
+
+    def on_key_release(self, key, modifiers):
+        if key == arcade.key.DOWN or key == arcade.key.S:
+            self.down_pressed = False
 
     def draw_grid(self, grid, offset_x, offset_y):
         """
@@ -337,12 +333,22 @@ class MyGame(arcade.Window):
 
                     arcade.draw_rectangle_filled(x, y, SIZE, SIZE, color)
 
-    # def draw_next(self, grid, offset_x, offset_y):
-    #     for row in range(len(grid)):
-    #         for column in range(len(grid[0])):
-    #             if grid[row][column]:
-    #                 color = colors[grid[row][column]]
-    #                 x =
+    def draw_next_box(self, grid, offset_x, offset_y):
+        for row in range(len(grid)):
+            for column in range(len(grid[0])):
+                if grid[row][column]:
+                    color = colors[grid[row][column]]
+                    x = 2 * PLAY_SCREEN_WIDTH + (MARGIN + SIZE) * (column + offset_x) + MARGIN + \
+                        SIZE // 2 + 2 * (SIZE + MARGIN)
+                    y = PLAY_SCREEN_HEIGHT - (MARGIN + SIZE) * (row + offset_y + 1) + \
+                        MARGIN + SIZE // 2 - 14 * (SIZE + MARGIN)
+
+                    arcade.draw_rectangle_filled(x, y, SIZE, SIZE, color)
+        arcade.draw_text("Next:",
+                         2 * PLAY_SCREEN_WIDTH + 2 * (SIZE + MARGIN) + MARGIN / 2,
+                         PLAY_SCREEN_HEIGHT - 13 * (SIZE + MARGIN) + MARGIN,
+                         arcade.color.WHITE,
+                         30, bold=True)
 
     def update_board(self):
         for row in range(len(self.board)):
@@ -357,7 +363,7 @@ class MyGame(arcade.Window):
         # Draws the falling tiles
         self.draw_grid(self.tile, self.tile_x, self.tile_y)
         # Draws the tiles in the next box
-        # self.draw_next(self.next_tile, self.next_tile_x, self.next_tile_y)
+        self.draw_next_box(self.next_tile, self.next_tile_x, self.next_tile_y)
         self.draw_counters()
 
     def draw_counters(self):
@@ -373,36 +379,36 @@ class MyGame(arcade.Window):
 
         # Level counter
         arcade.draw_text("Level:",
-                         2 * PLAY_SCREEN_WIDTH + SIZE,
+                         2 * PLAY_SCREEN_WIDTH + 2 * (SIZE + MARGIN) + MARGIN / 2,
                          PLAY_SCREEN_HEIGHT - 6 * SIZE - 5 * MARGIN,
                          arcade.color.WHITE,
                          30, bold=True)
         arcade.draw_text(f"{self.current_level}",
-                         2 * PLAY_SCREEN_WIDTH + SIZE,
+                         2 * PLAY_SCREEN_WIDTH + 2 * (SIZE + MARGIN) + MARGIN / 2,
                          PLAY_SCREEN_HEIGHT - 7 * SIZE - 7 * MARGIN,
                          arcade.color.WHITE,
                          30, bold=True)
 
         # Scoreboard
         arcade.draw_text("Score:",
-                         2 * PLAY_SCREEN_WIDTH + SIZE,
+                         2 * PLAY_SCREEN_WIDTH + 2 * (SIZE + MARGIN) + MARGIN / 2,
                          PLAY_SCREEN_HEIGHT - 3 * SIZE - 2 * MARGIN,
                          arcade.color.WHITE,
                          30, bold=True)
         arcade.draw_text(f"{self.score}",
-                         2 * PLAY_SCREEN_WIDTH + SIZE,
+                         2 * PLAY_SCREEN_WIDTH + 2 * (SIZE + MARGIN) + MARGIN / 2,
                          PLAY_SCREEN_HEIGHT - 4 * SIZE - 4 * MARGIN,
                          arcade.color.WHITE,
                          30, bold=True)
 
         if self.game_over:
-            arcade.draw_text("GAME",
-                             PLAY_SCREEN_WIDTH - SIZE - MARGIN,
+            arcade.draw_text(" GAME",
+                             PLAY_SCREEN_WIDTH - SIZE - 2 * MARGIN,
                              SCREEN_HEIGHT / 2,
                              arcade.color.RED,
                              100, bold=True)
-            arcade.draw_text("OVER",
-                             PLAY_SCREEN_WIDTH - SIZE - MARGIN,
+            arcade.draw_text(" OVER",
+                             PLAY_SCREEN_WIDTH - SIZE - 2 * MARGIN,
                              SCREEN_HEIGHT / 2 - 150,
                              arcade.color.RED,
                              100, bold=True)
--- Testing/test.py
+++ Testing/test.py
@@ -1,274 +1,105 @@
 import arcade
-import random
-import PIL
 
-# Set how many rows and columns we will have
-ROW_COUNT = 24
+
+# Set how many rows and columns we will have and how big each square will be
+ROW_COUNT = 21
 COLUMN_COUNT = 10
-
-# This sets the WIDTH and HEIGHT of each grid location
-WIDTH = 30
-HEIGHT = 30
-
-# This sets the margin between each cell
-# and on the edges of the screen.
+SIZE = 30
+# This sets the margin between each cell and edge of the screen
 MARGIN = 5
-
-# Do the math to figure out our screen dimensions
-SCREEN_WIDTH = (WIDTH + MARGIN) * COLUMN_COUNT + MARGIN
-SCREEN_HEIGHT = (HEIGHT + MARGIN) * ROW_COUNT + MARGIN
-SCREEN_TITLE = "Tetris"
-
-colors = [
-    (0,   0,   0, 255),
-    (255, 0,   0, 255),
-    (0,   150, 0, 255),
-    (0,   0,   255, 255),
-    (255, 120, 0, 255),
-    (255, 255, 0, 255),
-    (180, 0,   255, 255),
-    (0,   220, 220, 255)
-]
-
-# Define the shapes of the single parts
-tetris_shapes = [
-    [[1, 1, 1],
-     [0, 1, 0]],
-
-    [[0, 2, 2],
-     [2, 2, 0]],
-
-    [[3, 3, 0],
-     [0, 3, 3]],
-
-    [[4, 0, 0],
-     [4, 4, 4]],
-
-    [[0, 0, 5],
-     [5, 5, 5]],
-
-    [[6, 6, 6, 6]],
-
-    [[7, 7],
-     [7, 7]]
-]
+# How many different levels to choose to start from
+START_LEVELS_ROWS = 2
+START_LEVELS_COLUMNS = 5
+LEVEL_SELECT_SIZE = 2 * (SIZE * 2 + MARGIN)
+# Do the math to figure out the play screen dimensions and window dimensions
+PLAY_SCREEN_WIDTH = (SIZE + MARGIN) * COLUMN_COUNT + MARGIN
+PLAY_SCREEN_HEIGHT = (SIZE + MARGIN) * ROW_COUNT + MARGIN
+SCREEN_HEIGHT = (SIZE + MARGIN) * (ROW_COUNT + 3) + MARGIN
+SCREEN_WIDTH = 3 * PLAY_SCREEN_WIDTH
 
 
-def create_textures():
-    """ Create a list of images for sprites based on the global colors. """
-    new_textures = []
-    for color in colors:
-        # noinspection PyUnresolvedReferences
-        image = PIL.Image.new('RGBA', (WIDTH, HEIGHT), color)
-        new_textures.append(arcade.Texture(str(color), image=image))
-    return new_textures
-
-
-texture_list = create_textures()
-
-
-def rotate_counterclockwise(shape):
-    """ Rotates a matrix clockwise """
-    return [[shape[y][x] for y in range(len(shape))]
-            for x in range(len(shape[0]) - 1, -1, -1)]
-
-
-def check_collision(board, shape, offset):
+class LevelSelect(arcade.Window):
     """
-    See if the matrix stored in the shape will intersect anything
-    on the board based on the offset. Offset is an (x, y) coordinate.
+    Main application class.
     """
-    off_x, off_y = offset
-    for cy, row in enumerate(shape):
-        for cx, cell in enumerate(row):
-            if cell and board[cy + off_y][cx + off_x]:
-                return True
-    return False
 
-
-def remove_row(board, row):
-    """ Remove a row from the board, add a blank row on top. """
-    del board[row]
-    return [[0 for _ in range(COLUMN_COUNT)]] + board
-
-
-def join_matrixes(matrix_1, matrix_2, matrix_2_offset):
-    """ Copy matrix 2 onto matrix 1 based on the passed in x, y offset coordinate """
-    offset_x, offset_y = matrix_2_offset
-    for cy, row in enumerate(matrix_2):
-        for cx, val in enumerate(row):
-            matrix_1[cy + offset_y - 1][cx + offset_x] += val
-    return matrix_1
-
-
-def new_board():
-    """ Create a grid of 0's. Add 1's to the bottom for easier collision detection. """
-    # Create the main board of 0's
-    board = [[0 for _x in range(COLUMN_COUNT)] for _y in range(ROW_COUNT)]
-    # Add a bottom border of 1's
-    board += [[1 for _x in range(COLUMN_COUNT)]]
-    return board
-
-
-class MyGame(arcade.Window):
-    """ Main application class. """
-
-    def __init__(self, width, height, title):
-        """ Set up the application. """
-
-        super().__init__(width, height, title)
-
-        arcade.set_background_color(arcade.color.WHITE)
-
-        self.board = None
-        self.frame_count = 0
-        self.game_over = False
-        self.paused = False
-        self.board_sprite_list = None
-
-        self.stone = None
-        self.stone_x = 0
-        self.stone_y = 0
-
-    def new_stone(self):
+    def __init__(self, width, height):
         """
-        Randomly grab a new stone and set the stone location to the top.
-        If we immediately collide, then game-over.
+        Set up the application.
         """
-        self.stone = random.choice(tetris_shapes)
-        self.stone_x = int(COLUMN_COUNT / 2 - len(self.stone[0]) / 2)
-        self.stone_y = 0
+        super().__init__(width, height)
+        # Create a 2 dimensional array. A two dimensional
+        # array is simply a list of lists.
+        self.grid = []
+        for row in range(START_LEVELS_ROWS):
+            # Add an empty array that will hold each cell
+            # in this row
+            self.grid.append([])
+            for column in range(START_LEVELS_COLUMNS):
+                self.grid[row].append(0)  # Append a cell
 
-        if check_collision(self.board, self.stone, (self.stone_x, self.stone_y)):
-            self.game_over = True
-
-    def setup(self):
-        self.board = new_board()
-
-        self.board_sprite_list = arcade.SpriteList()
-        for row in range(len(self.board)):
-            for column in range(len(self.board[0])):
-                sprite = arcade.Sprite()
-                for texture in texture_list:
-                    sprite.append_texture(texture)
-                sprite.set_texture(0)
-                sprite.center_x = (MARGIN + WIDTH) * column + MARGIN + WIDTH // 2
-                sprite.center_y = SCREEN_HEIGHT - (MARGIN + HEIGHT) * row + MARGIN + HEIGHT // 2
-
-                self.board_sprite_list.append(sprite)
-
-        self.new_stone()
-        self.update_board()
-
-    def drop(self):
-        """
-        Drop the stone down one place.
-        Check for collision.
-        If collided, then
-          join matrixes
-          Check for rows we can remove
-          Update sprite list with stones
-          Create a new stone
-        """
-        if not self.game_over and not self.paused:
-            self.stone_y += 1
-            if check_collision(self.board, self.stone, (self.stone_x, self.stone_y)):
-                self.board = join_matrixes(self.board, self.stone, (self.stone_x, self.stone_y))
-                while True:
-                    for i, row in enumerate(self.board[:-1]):
-                        if 0 not in row:
-                            self.board = remove_row(self.board, i)
-                            break
-                    else:
-                        break
-                self.update_board()
-                self.new_stone()
-
-    def rotate_stone(self):
-        """ Rotate the stone, check collision. """
-        if not self.game_over and not self.paused:
-            new_stone = rotate_counterclockwise(self.stone)
-            if self.stone_x + len(new_stone[0]) >= COLUMN_COUNT:
-                self.stone_x = COLUMN_COUNT - len(new_stone[0])
-            if not check_collision(self.board, new_stone, (self.stone_x, self.stone_y)):
-                self.stone = new_stone
-
-    def on_update(self, dt):
-        """ Update, drop stone if warrented """
-        self.frame_count += 1
-        if self.frame_count % 10 == 0:
-            self.drop()
-
-    def move(self, delta_x):
-        """ Move the stone back and forth based on delta x. """
-        if not self.game_over and not self.paused:
-            new_x = self.stone_x + delta_x
-            if new_x < 0:
-                new_x = 0
-            if new_x > COLUMN_COUNT - len(self.stone[0]):
-                new_x = COLUMN_COUNT - len(self.stone[0])
-            if not check_collision(self.board, self.stone, (new_x, self.stone_y)):
-                self.stone_x = new_x
-
-    def on_key_press(self, key, modifiers):
-        """
-        Handle user key presses
-        User goes left, move -1
-        User goes right, move 1
-        Rotate stone,
-        or drop down
-        """
-        if key == arcade.key.LEFT:
-            self.move(-1)
-        elif key == arcade.key.RIGHT:
-            self.move(1)
-        elif key == arcade.key.UP:
-            self.rotate_stone()
-        elif key == arcade.key.DOWN:
-            self.drop()
-
-    # noinspection PyMethodMayBeStatic
-    def draw_grid(self, grid, offset_x, offset_y):
-        """
-        Draw the grid. Used to draw the falling stones. The board is drawn
-        by the sprite list.
-        """
-        # Draw the grid
-        for row in range(len(grid)):
-            for column in range(len(grid[0])):
-                # Figure out what color to draw the box
-                if grid[row][column]:
-                    color = colors[grid[row][column]]
-                    # Do the math to figure out where the box is
-                    x = (MARGIN + WIDTH) * (column + offset_x) + MARGIN + WIDTH // 2
-                    y = SCREEN_HEIGHT - (MARGIN + HEIGHT) * (row + offset_y) + MARGIN + HEIGHT // 2
-
-                    # Draw the box
-                    arcade.draw_rectangle_filled(x, y, WIDTH, HEIGHT, color)
-
-    def update_board(self):
-        """
-        Update the sprite list to reflect the contents of the 2d grid
-        """
-        for row in range(len(self.board)):
-            for column in range(len(self.board[0])):
-                v = self.board[row][column]
-                i = row * COLUMN_COUNT + column
-                self.board_sprite_list[i].set_texture(v)
+        arcade.set_background_color(arcade.color.BLACK)
 
     def on_draw(self):
-        """ Render the screen. """
+        """
+        Render the screen.
+        """
 
         # This command has to happen before we start drawing
         arcade.start_render()
-        self.board_sprite_list.draw()
-        self.draw_grid(self.stone, self.stone_x, self.stone_y)
+
+        # Draw TETRIS and ask what level the player wants to start on
+        arcade.draw_text("TETRIS",
+                         PLAY_SCREEN_WIDTH - LEVEL_SELECT_SIZE / 2,
+                         PLAY_SCREEN_HEIGHT - SIZE,
+                         arcade.color.WHITE,
+                         100)
+        arcade.draw_text("Select a level to start on:",
+                         2 * MARGIN + LEVEL_SELECT_SIZE,
+                         2 * SCREEN_HEIGHT / 3,
+                         arcade.color.WHITE,
+                         50)
+
+        # Draw the grid
+        for row in range(START_LEVELS_ROWS):
+            for column in range(START_LEVELS_COLUMNS):
+                # Figure out what color to draw the box
+                if self.grid[row][column] == 1:
+                    color = arcade.color.GREEN
+                else:
+                    color = arcade.color.WHITE
+
+                # Do the math to figure out where the box is
+                x = (MARGIN + LEVEL_SELECT_SIZE) * column + MARGIN + LEVEL_SELECT_SIZE // 2 + PLAY_SCREEN_WIDTH / 2
+                y = (MARGIN + LEVEL_SELECT_SIZE) * row + MARGIN + LEVEL_SELECT_SIZE // 2 + SCREEN_HEIGHT / 4
+
+                # Draw the box
+                arcade.draw_rectangle_filled(x, y, LEVEL_SELECT_SIZE, LEVEL_SELECT_SIZE, color)
+
+                # Draw the number in the boxes
+                arcade.draw_text(f"{5 * (-row + 1) + column}",
+                                 x - 2 * MARGIN,
+                                 y - 2 * MARGIN,
+                                 arcade.color.BLACK,
+                                 30, bold=True)
+
+    def on_mouse_press(self, x, y, button, modifiers):
+        """
+        Called when the user presses a mouse button.
+        """
+
+        # Change the x/y screen coordinates to grid coordinates
+        column = (x - PLAY_SCREEN_WIDTH // 2) // (LEVEL_SELECT_SIZE + MARGIN)
+        row = (y - PLAY_SCREEN_HEIGHT // 4) // (LEVEL_SELECT_SIZE + MARGIN)
+
+        if -1 < column < START_LEVELS_COLUMNS and -1 < row < START_LEVELS_ROWS:
+            level = 5 * (-row + 1) + column
+            print(level)
 
 
 def main():
-    """ Create the game window, setup, run """
-    my_game = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-    my_game.setup()
+
+    window = LevelSelect(SCREEN_WIDTH, SCREEN_HEIGHT)
     arcade.run()
 
 
